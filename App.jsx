32ce36b573510ab79f196deb659109616cf4940d
Teja <23pa1a1261@vishnu.edu.in>
2025-09-08 23:28:28 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import Test from './Test'
+import Loginpage from './login_page/Loginpage.jsx'
+import './App.css'
+function App() {
+
+  return (
+    <div>
+
+      <Loginpage />
+    </div>
+  )
+}
+
+export default App
