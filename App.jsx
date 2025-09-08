ff14bf8b94ac85222ecaa9a13b4b72746af82d0f
Teja <23pa1a1261@vishnu.edu.in>
2025-09-08 23:43:13 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Test from './Test'
+import UseState from './UseState'
+import imgTeja from './assets/teja.jpg'
+import Manage from './Manage'
+import Homepage from './pokemon/Homepage'
+import Tictactoe from './tic-tac-toe/Tictactoe';
+import CountIncrement from './usestate_practice/CountIncrement.jsx'
 import Loginpage from './login_page/Loginpage.jsx'
 import './App.css'
 function App() {
-
+  // let mydetails = {
+  //   name: "Rahul",
+  //   age: 25,
+  //   city: "Bangalore"
+  // }
+  // return (
+  //   <div>
+  //     <Manage />
+  //   </div>
+  // )
   return (
     <div>
 
